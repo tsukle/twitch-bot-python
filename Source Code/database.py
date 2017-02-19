0212c9d05cfe215967e0bcd0a6e47db97b419af5
Emilis Tobulevicius <etobulevicius@hotmail.co.uk>
2017-02-19 23:23:31 +0000

    init

---
+++ Source Code/database.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+db = sqlite3.connect("database.db")
+c = db.cursor()
+
+#This creates the commands table if it does not exist
+def createTable():
+    c.execute("CREATE TABLE IF NOT EXISTS Commands(command TEXT, response TEXT)")
+
+#query adds command/response to the commands table
+def addCommand(com, response):
+    try:
+        c.execute("INSERT INTO Commands (command, response) VALUES (?, ?)", (com, response))
+        db.commit()
+        return 1
+    except sqlite3.OperationalError as e:
+        return e
+
+#query removes command/response to the commands table
+def removeCommand(dcom):
+    try:
+        c.execute("DELETE FROM Commands WHERE command=?", (dcom,))
+        db.commit()
+        return 1
+    except sqlite3.OperationalError as e:
+        return e
+
+#query fetches response on request of a command
+def getCommand(com):
+    c.execute("SELECT * FROM Commands WHERE command = ?", (com,))
+    response = c.fetchone()
+    if response is not None:
+        return response[1]
+    else:
+        return 0
+
+#query fetches response on request of a command
+def getCommandList():
+    x = "Commands: "
+    c.execute("SELECT command FROM Commands")
+    d = c.fetchall()
+    for index, com in enumerate(d):
+        res = com[0]
+        if(index >= len(d) - 1):
+            x = x + res
+            return x
+        else:
+            x = x + res + "  |  "
---
+++ Source Code/events.py
@@ -0,0 +1,120 @@
+import socket
+import json
+import database
+from colorama import init, Fore, Back, Style
+from time import sleep
+
+init(autoreset=True)
+s = socket.socket()
+db = database
+
+with open("settings.json") as data:
+    settings = json.load(data)
+
+def setup():
+    print(Fore.WHITE + """
+                   Twitch Chat Bot                 
+            By: Tsukle (http://tsukle.com)         
+                    Version: 1.0
+              (Don't resize the window.)""")                   
+    print(Fore.WHITE + "____________________________________________________")
+
+    print("")
+
+#Connects to the twitch IRC and logs in also giving the channel name to join.
+def createSocket():
+    s.connect(("irc.twitch.tv", 6667))
+    s.send(("PASS " + settings["authkey"] + "\r\n").encode())
+    s.send(("NICK " + settings["username"] + "\r\n").encode())
+    s.send(("JOIN #" + settings["channel"] + "\r\n").encode())
+    return s
+
+#Bot sends a normal message to chat
+def sendMessage(Message):
+    Message = str(Message)
+    msgConstruct = "PRIVMSG #" + settings["channel"] + " :" + Message
+    s.send((msgConstruct + "\r\n").encode())
+    print(Fore.BLACK + Back.YELLOW + " BOT " + Style.RESET_ALL + " > " + Message)
+    sleep(1)
+
+#Bot sends a special (coloured) message to chat
+def sendSpecialMessage(Message):
+    Message = str(Message)
+    msgConstruct = "PRIVMSG #" + settings["channel"] + " :/me " + Message
+    s.send((msgConstruct + "\r\n").encode())
+    print(Fore.BLACK + Back.YELLOW + " BOT " + Style.RESET_ALL + " > " + Message)
+    sleep(1)
+
+#AFK response
+def AFKCheck():
+    msgConstruct = "PONG :tmi.twitch.tv"
+    s.send((msgConstruct + "\r\n").encode())
+    print(Fore.BLACK + Back.CYAN + " INFO " + Style.RESET_ALL + " > AFK Check complete ")
+
+#Takes IRC line and splits it | returns name
+def getUser(msg):
+    spliced = msg.split(":", 2) #1: gets rid of the first colon it finds    2: gets rid of the first two it finds and splits it
+    user = spliced[1].split("!", 1)[0]
+    if("tmi.twitch.tv" in user):
+        user = "twitch"
+        return user
+    elif("tsuklebot" in user):
+        user = "bot"
+        return user
+    else:
+        return user
+
+
+#Takes IRC line and splits it | returns message
+def getMessage(msg):
+    if(".tmi.twitch.tv JOIN" in msg):
+        message = "x5edtxmWKwx5ed"
+        return str(message)
+
+    elif("End of /NAMES list" in msg):
+        message = "x5edtxmWKw"
+        return message
+
+    elif("PING :tmi.twitch.tv" in msg):
+        AFKCheck()
+
+    else:
+        spliced = msg.split(":", 2) #1: gets rid of the first colon it finds    2: gets rid of the first two it finds and splits it
+        message = spliced[2]
+        return str(message)
+
+#Splices owner command and adds new command to database
+def createCommand(msg):
+    spliced = msg.split(" ", 2)
+    command = spliced[1]
+    response = spliced[2]
+    if("!commands" in command or "!addcom" in command or "!delcom" in command):
+        print(Fore.BLACK + Back.CYAN + " INFO " + Style.RESET_ALL + " > Command: " + command + " - Is a reserved command.")
+    else:    
+        dbResponse = db.addCommand(command, response)
+        if (dbResponse != 0):
+            print(Fore.BLACK + Back.CYAN + " INFO " + Style.RESET_ALL + " > Command: " + command + " - Added to the database.")
+        else:
+            print(Fore.BLACK + Back.CYAN + " INFO " + Style.RESET_ALL + " > Error: " + str(dbResponse))
+
+#Splices owner command and removes the command from the database
+def deleteCommand(msg):
+    spliced = msg.split(" ", 1)
+    s2 = spliced[1]
+    s3 = s2.split("\r", 2)
+    command = s3[0]
+    dbRes = db.removeCommand(command)
+    if (dbRes == 1):
+        print(Fore.BLACK + Back.CYAN + " INFO " + Style.RESET_ALL + " > Command: " + command + " - Removed from the database.")
+    else:
+        print(Fore.BLACK + Back.CYAN + " INFO " + Style.RESET_ALL + " > Error: " + str(dbRes))
+
+#Splices command and searches database for it
+def execCommand(msg):
+    spliced = msg.split("\r", 2)
+    command = spliced[0]
+    response = db.getCommand(command)
+    if (response != 0):
+        sendMessage(response)
+    else:
+        print(Fore.BLACK + Back.CYAN + " INFO " + Style.RESET_ALL + " > Command: " + command + " - Not found in the database.")
